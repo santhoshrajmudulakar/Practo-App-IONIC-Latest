c963683fbf4eb5a29f228e5ab46dfd5627eb04e6
santhosh <santhosh@azrisolutions.com>
2017-12-26 00:06:50 +0530

    height issue

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,14 +1,14 @@
 
- import { ModuleWithProviders } from '@angular/core';
- import { Routes, RouterModule } from '@angular/router';
+//  import { ModuleWithProviders } from '@angular/core';
+//  import { Routes, RouterModule } from '@angular/router';
 
  
 
- const routes: Routes = [
+//  const routes: Routes = [
     
-     { path: 'components/home', component : HomeComponent }
+//      { path: 'components/home', component : HomeComponent }
     
      
- ];
+//  ];
 
-// export const routing: ModuleWithProviders = RouterModule.forRoot(routes); 
+// // export const routing: ModuleWithProviders = RouterModule.forRoot(routes); 
