27bb83f567b4b868f5fb3472f5f8d166d01aac25
santhosh <santhosh@azrisolutions.com>
2018-01-10 01:24:51 +0530

    build

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { ViewChild } from '@angular/core';
+import { Slides } from 'ionic-angular';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  @ViewChild(Slides) slides: Slides;
+  
 
   constructor(public navCtrl: NavController) {
 
   }
- 
+   goToSlide() {
+    this.slides.slideTo(3, 10);
+  }
+  slideChanged() {
+    let currentIndex = this.slides.getActiveIndex();
+   
+  }
+   ngAfterViewInit() {
+    this.slides.freeMode = true;
+  }
+  
 }
